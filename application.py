b65832f670153a68d7317121071a26d89fd2265c
doug <drehnstrom@yahoo.com>
2017-09-04 20:39:09 -0400

    MAde Multi-Threaded

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def main():
-    model = {"title":"Welcome to the GREAT CONVERTER!"}
+    model = {"title":"Welcome to the GREAT CONVERTER!!"}
     return render_template('index.html', model=model)
 
 
@@ -83,4 +83,4 @@ def handle_invalid_usage(error):
 
 # Need to add  port=8080,  in run function below to run on ELB
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8080, debug=True)
+    application.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
